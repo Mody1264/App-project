3e0fe2cf4aa5e501644a0336880675e509633cff
Mody 5 <morrismody12647@gmail.com>
2017-08-19 20:45:31 +0800

    2
    Creat

---
+++ JS/Create.js
@@ -0,0 +1,27 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("createCollotion");
+var createCollotion = db.collection('create');
+
+createCollotion.load()
+
+$("#submit").click(function () {
+var date = $("#date").val()
+var kind = $("#kind").val()
+var option = $("#option").val()
+var amount = $("#amount").val()
+
+createCollotion.insert({
+date: date,
+kind: kind,
+option: option,
+amount,
+});
+createCollotion.save()
+alert("complete")
+
+$("#date").val("")
+$("#kind").val("")
+$("#option").val("")
+$("#amount").val("")
+
+});
