cbdb121ace4ac6028ef86a90a661a9403128ba05
Mody 5 <morrismody12647@gmail.com>
2017-09-09 21:06:26 +0800

    7
    all

--- JS/lookup.js
+++ JS/lookup.js
@@ -8,7 +8,7 @@ function createAccontingHTMLString(date, kind, option, amount) {
 }
 
 $("#Searchbtn").click(function() {
-    $("#creattable").find("tr").remove();
+    $("#creattable").find("tbody").remove();
     if ($('input[name=timerange]:checked').val() == "month") {
         var date = new Date();
         var year = date.getUTCFullYear();
@@ -17,7 +17,7 @@ $("#Searchbtn").click(function() {
             month = "0" + month;
         }
         var dateString = year + "-" + month + "-" + "01";
-        
+
         var create = createCollotion.find({
             date: {
                 $gte: dateString
@@ -27,8 +27,36 @@ $("#Searchbtn").click(function() {
         for (var i = 0; i < create.length; i++) {
             $("#creattable").append(createAccontingHTMLString(create[i].date, create[i].kind, create[i].option, create[i].amount))
         }
+        var eatCost = 0;
+        var playCost = 0;
+        var otherCost = 0;
+        for (var i = 0; i < create.length; i++) {
+            console.log(create[i].kind)
+            if (create[i].kind == "Food") {
+                console.log(create[i].amount)
+                eatCost += create[i].amount / 1;
+            } else if (create[i].kind == "Play") {
+                playCost += create[i].amount / 1;
+            } else if (create[i].kind == "Others") {
+                otherCost += create[i].amount / 1;
+            }
+        }
+        console.log(eatCost)
+        var totalCost = eatCost + playCost + otherCost;
+        var eatProportion = Math.round((eatCost / totalCost) * 100) + "%";
+        var playProportion = Math.round((playCost / totalCost) * 100) + "%";
+        var otherProportion = Math.round((otherCost / totalCost) * 100) + "%";
+
+        $("#eatCost").text(eatCost)
+        $("#eatProportion").text(eatProportion)
+        $("#playCost").text(playCost)
+        $("#playProportion").text(playProportion)
+        $("#otherCost").text(otherCost)
+        $("#otherProportion").text(otherProportion)
+        $("#totalCost").text(totalCost)
     } else {
-        var timestart = $("#timestart").val()
+        var timestart = $("#totalstart").val()
+
         var timeend = $("#timeend").val()
         console.log(timestart)
         var create = createCollotion.find({
